23b2214a4203ce33180e5eeab9fbb7c123f1de43
Heo Joon Woo <hensher5050@gmail.com>
2025-09-18 16:46:32 +0900

    sooo hard but i did it

--- src/filters.py
+++ src/filters.py
@@ -4,25 +4,46 @@ import numpy as np
 
 class Filters:
     # TODO: Image kernels
-    Kernels = {
+    Kernels = {"Original" : np.array([[0,0,0], [0,1,0], [0,0,0]], dtype=np.float32),
+               "Blur" : np.array([[1,1,1], [1,1,1], [1,1,1]], dtype=np.float32) / 9,
+               "Gaussian blur" : np.array([[1,2,1], [2,4,2], [1,2,1]], dtype=np.float32) / 16,
+               "Sharpen" : np.array([[0,-1,0], [-1,5,-1], [0,-1,0]], dtype=np.float32),
+               "Sobel (x)" : np.array([[-1,0,1], [-2,0,2], [-1,0,1]], dtype=np.float32),
+               "Sobel (y)" : np.array([[-1,-2,-1], [0,0,0], [1,2,1]], dtype=np.float32),
+               "Edge detection" : np.array([[-1,-1,-1], [-1,8,-1], [-1,-1,-1]], dtype=np.float32),
+               "Emboss" : np.array([[-2,-1,0], [-1,1,1], [0,1,2]], dtype=np.float32)
     }
 
     def __init__(self, kernels=Kernels):
         self.kernels = kernels
         # TODO: Implement internal variables
+        self.kernel_names = list(self.kernels.keys())
+        self.filter_index = 0
 
     def apply_filter(self, frame, filter_name) -> np.array:
         # TODO: Apply the selected filter kernel to the frame
-        pass
+        if filter_name not in self.kernels:
+            return frame
+        kernel = self.kernels[filter_name]
+        return cv2.filter2D(frame, -1, kernel)
 
     def get_current_filter_name(self) -> str:
         # TODO: Return currently set kernels's name
-        pass
+        return self.kernel_names[self.filter_index]
 
     def switch_next_filter(self):
         # TODO: Update currently selected kernel to the next
-        pass
-
+        if self.filter_index +1 >= len(self.kernel_names):
+            self.filter_index = 0
+        else:
+            self.filter_index += 1
+        kernel = self.kernel_names[self.filter_index ]
+        return kernel
     def switch_previous_filter(self):
         # TODO: Update currently selected kernel to the previous
-        pass
+        if self.filter_index -1 < 0:
+            self.filter_index = len(self.kernel_names) -1
+        else:
+            self.filter_index -= 1
+        kernel = self.kernel_names[self.filter_index ]
+        return kernel
